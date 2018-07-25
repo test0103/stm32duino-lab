d21bd3104e1e4998eaabe9afca47d7aba10d49ce
llz123 <llz123@outlook.com>
2018-07-26 00:43:51 +0800

    Add files via upload

---
+++ stm32.ino
@@ -0,0 +1,27 @@
+#include <Wire_slave.h>
+#define SLAVE_ADDRESS 0x04
+int data;
+void setup() {
+Serial.begin(9600); // start serial for output
+// initialize i2c as slave
+Wire.begin(SLAVE_ADDRESS);
+// define callbacks for i2c communication
+Wire.onReceive(receiveData);//从机 接收 主机 发来的数据
+Wire.onRequest(sendData); //从机 请求 主机 发送数据
+Serial.println("Ready");
+}
+void loop() {
+ delay(100);
+ }
+// callback for received data
+void receiveData(int byteCount){
+while(Wire.available()) {
+data = Wire.read();
+Serial.print("data received: ");
+Serial.println(data);
+}
+}
+// callback for sending data
+ void sendData(){
+ Wire.write(data);
+ }
